0e1de2814af64291b6a7645393ede8afb1e3ec6e
Ivan Miranda <ivan.i.n@hotmail.com>
2016-03-27 13:43:21 -0300

    project structure

---
+++ banco-questoes/client/app/app.js
@@ -0,0 +1,4 @@
+(function () {
+    'use strict';
+    angular.module('banco-questoes', ['angular-meteor', 'accounts.ui', 'angular-meteor.auth']);
+}());
---
+++ banco-questoes/client/exemple-controller.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict';
+
+    angular.module('banco-questoes').controller('ExempleController', ['$scope', function ($scope) {
+
+    }]);
+}());
